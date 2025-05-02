44c9e6c126b05e01c8e155f95af283d033e7f1ac
65070129 <65070129@kmitl.ac.th>
2025-05-02 11:06:52 +0700

    update sidebar

--- app/components/sidebar.js
+++ app/components/sidebar.js
@@ -4,13 +4,13 @@ const Sidebar = () => {
         <nav>
           <ul className="space-y-2">
             <li>
-              <a href="#" className="block p-2 hover:bg-gray-700 rounded">Home</a>
+              <a href="#" className="block p-2 hover:bg-gray-700 rounded">หน้าหลัก</a>
             </li>
             <li>
-              <a href="#" className="block p-2 hover:bg-gray-700 rounded">Profile</a>
+              <a href="#" className="block p-2 hover:bg-gray-700 rounded">บัญชีที่ 1</a>
             </li>
             <li>
-              <a href="#" className="block p-2 hover:bg-gray-700 rounded">Settings</a>
+              <a href="#" className="block p-2 hover:bg-gray-700 rounded">บัญชีที่ 2</a>
             </li>
             <li>
               <a href="#" className="block p-2 hover:bg-gray-700 rounded">Logout</a>
